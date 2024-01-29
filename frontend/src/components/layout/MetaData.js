8ef08f7e9646f401d1e5cb959e4bb2e31ae19ca8
LouisMc1828 <louisf1828@outlook.com>
2024-01-29 12:28:11 -0400

    Seccion 16 - Layout & Home

---
+++ frontend/src/components/layout/MetaData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+const MetaData = ({titulo}) => {
+  return (
+    <Helmet>
+        <title>{`${titulo} - Sell-iFy Ecommerce`}</title>
+    </Helmet>
+  )
+}
+
+export default MetaData
